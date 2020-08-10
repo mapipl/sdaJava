1d00b1b1b35042a30888ac48a7261b17f5c486f0
Maciek P <maciek_p@interia.pl>
2020-08-10 22:12:33 +0200

    java_programing_5 part 01

---
+++ src/main/java/java_programing_5/ForStatement.java
@@ -0,0 +1,40 @@
+package java_programing_5;
+
+public class ForStatement {
+
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 8; i++) {
+            System.out.println("10.000 at " + i + "% interest = " + Math.round(calculateInterest(10000, i)));
+        }
+
+        int count = 0;
+        for (int i = 10; i<50; i++){
+            if (isPrime(i)){
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if (count==3){
+                    System.out.println("Exiting for loop");
+                    break;
+                }
+            }
+        }
+    }
+
+    public static double calculateInterest(double amount, double interestRate) {
+        return (amount * (interestRate / 100));
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/java_programing_5/Switch.java
@@ -0,0 +1,69 @@
+package java_programing_5;
+
+public class Switch {
+
+    public static void main(String[] args) {
+
+        int value = 1;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2) {
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Was not 1 or 2");
+        }
+
+        int switchValue = 4;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Was 3, or 4, or 5");
+                System.out.println("Actually it was " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+        printDayOfTheWeek(-1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(5);
+    }
+
+//        --- DAY OF THE WEEK ---
+
+    public static void printDayOfTheWeek(int day) {
+        switch (day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
