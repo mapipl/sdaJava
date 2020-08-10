f59d34aab192c5706b9cf54d13c7aa7e7ef0bc7c
Maciek P <maciek_p@interia.pl>
2020-08-10 13:56:25 +0200

    java_programing_3 part 02

--- src/main/java/java_programing_3/MethodOverloading.java
+++ src/main/java/java_programing_3/MethodOverloading.java
@@ -7,6 +7,14 @@ public class MethodOverloading {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+
+//        --- FEET, INCHES, CENTIMETERS ---
+
+        double centrimeters = calcFeetAndInchesToCentimeters(7, 5);
+        if (centrimeters < 0.0) {
+            System.out.println("Invalid parameters");
+        }
+        calcFeetAndInchesToCentimeters(100);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -23,5 +31,28 @@ public class MethodOverloading {
         System.out.println("No player name, no player score");
         return 0;
     }
-    
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        }
+
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            System.out.println("Invalid inches parameter");
+            return -1;
+        }
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to " + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
 }
