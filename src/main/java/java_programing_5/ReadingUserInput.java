dcc5c4aba8f74b99872611d3a76cbf2a1ae4fbc4
Maciek P <maciek_p@interia.pl>
2020-08-16 09:28:33 +0200

    java_programing_5 part 03

---
+++ src/main/java/java_programing_5/ReadingUserInput.java
@@ -0,0 +1,39 @@
+package java_programing_5;
+
+import java.util.Scanner;
+
+public class ReadingUserInput {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+        while (true) {
+
+            int order = counter + 1;
+            System.out.println("Enter number #" + order + ":");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+                if (counter == 10) {
+                    break;
+                }
+            } else {
+                System.out.println("Invalid number");
+            }
+
+            scanner.nextLine(); // handle enter key
+        }
+        System.out.println("sum = " + sum);
+
+        scanner.close();
+    }
+
+}
