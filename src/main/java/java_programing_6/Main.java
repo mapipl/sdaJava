63e7a7de218a45781b47e9229b66417acea27aa3
Maciek P <maciek_p@interia.pl>
2020-08-18 10:36:16 +0200

    java_programing_6 part 01

---
+++ src/main/java/java_programing_6/Main.java
@@ -0,0 +1,17 @@
+package java_programing_6;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Outlander outlander = new Outlander(36);
+        outlander.steer(45);
+        outlander.accelerate(30);
+        outlander.accelerate(20);
+        outlander.accelerate(-42);
+
+
+
+    }
+
+}
